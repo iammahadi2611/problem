325112576e0ae81ba20a99d1fa179f8fe1330b0a
mahadi2611 <mahadi2611@yahoo.com>
2023-01-08 11:26:26 +0600

    one problem solved

--- ext/Script.js
+++ ext/Script.js
@@ -1,21 +1,47 @@
-let minusBtn = document.querySelector('.minusBtn');
-let plusBtn = document.querySelector('.plusBtn');
-let numItemS = document.querySelector('.numItem').innerText;
-let numItem = parseFloat(numItemS) ;
-// let priceTag = parseFloat(document.querySelector('.priceTag').innerText);
+let minusBtnA = document.querySelector('.minusBtnA');
+let minusBtnB = document.querySelector('.minusBtnB');
+let plusBtnA = document.querySelector('.plusBtnA');
+let plusBtnB = document.querySelector('.plusBtnB');
+let numItemA = parseFloat(document.querySelector('.numItemA').innerText) ;
+let numItemB = parseFloat(document.querySelector('.numItemB').innerText) ;
+let priceTagA = parseFloat(document.querySelector('.priceTagA').innerText);
+let priceTagB = parseFloat(document.querySelector('.priceTagB').innerText);
 
-plusBtn.addEventListener('click', function(){
-    document.querySelector('.numItem').innerText = numItem++ ;
-    document.querySelector('.numItem').innerText;
-    console.log(numItem)
-})
-minusBtn.addEventListener('click', function(){
-    document.querySelector('.numItem').innerText = numItem--;
-    document.querySelector('.numItem').innerText;
-    if(numItem == 0){
-        document.querySelector('.minusBtn').addEventListener('click', function(){
-        })
-    }
-    console.log(numItem)
-})
+//Himu Series Book Javascript 
+    plusBtnA.addEventListener('click', function(){
+
+        document.querySelector('.numItemA').innerText = numItemA ++;  //problem with button response
+    
+        document.querySelector('.priceTagA').innerText = priceTagA * numItemA;
+    
+    
+        console.log('numItemA',numItemA);
+    
+    })
+    minusBtnA.addEventListener('click', function(){
+        document.querySelector('.numItemA').innerText = numItemA --;  //problem with button response
+    
+        document.querySelector('.priceTagA').innerText = priceTagA * numItemA ; 
+
+        console.log('numItemA',numItemA);
+    })
+//Sherlock Holmes Book Javascript
+    plusBtnB.addEventListener('click', function(){
+
+        document.querySelector('.numItemB').innerText = numItemB ++;  //problem with button response
+    
+        document.querySelector('.priceTagB').innerText = priceTagB * numItemB;
+    
+    
+        console.log('numItemB',numItemB);
+    
+    })
+    minusBtnB.addEventListener('click', function(){
+        document.querySelector('.numItemB').innerText = numItemB --;  //problem with button response
+    
+        document.querySelector('.priceTagB').innerText = priceTagB * numItemB ; 
+        
+        console.log('numItemB',numItemB);
+    })
+
 
